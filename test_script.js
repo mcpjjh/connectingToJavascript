66fcb5e8acc981b560cd8b9ca45b459b3046f584
Young <mcpjjh@gmail.com>
2017-01-31 18:55:26 +0000

    wrote lookup script to query DB

---
+++ test_script.js
@@ -0,0 +1,24 @@
+const pg = require("pg");
+const settings = require("./settings");
+
+const client = new pg.Client({
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+});
+
+const queryDB = (q, params, cb) => {
+  client.connect((err) => {
+    if (err) {
+      return console.error("Connection Error", err);
+    }
+  client.query (q, params, cb)
+  })
+};
+
+module.exports = {
+  query: queryDB
+};
